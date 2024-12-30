9ef27d08fc22b57dd8017e4201ee672f5f9eafef
euxn23 <euxn23@gmail.com>
2024-12-30 17:39:57 +0900

    feat: update other works

--- other-works.ts
+++ other-works.ts
@@ -1,11 +1,29 @@
-type Entry = {
+type Work = {
   date: string;
   title: string;
   media: string;
   url: string;
 };
 
-const DW_ENTRIES: Entry[] = [
+const OTHER_WORKS: Work[] = [
+  {
+    date: '2024-12-01',
+    title: 'OpenAPI Spec を出力できる DSL、TypeSpec の実践例',
+    media: 'ドワンゴ教育サービス開発者ブログ',
+    url: 'https://blog.nnn.dev/entry/2024/12/01/210000',
+  },
+  {
+    date: '2024-11-16',
+    title: '型付き API リクエストを実現するいくつかの手法とその選択',
+    media: 'TSKaigi Kansai 2024',
+    url: 'https://speakerdeck.com/euxn23/typed-api-request',
+  },
+  {
+    date: '2024-06-28',
+    title: 'TypeSpec を使い倒してる',
+    media: 'Kyoto.js 22',
+    url: 'https://speakerdeck.com/euxn23/use-up-typespec',
+  },
   {
     date: '2024-05-11',
     title: 'Powerfully Typed TypeScript',
@@ -32,7 +50,7 @@ export async function getOtherWorks() {
   const { articles: zennArticles } = await fetch(
     "https://zenn.dev/api/articles?username=euxn23&order=latest"
   ).then((res) => res.json());
-  const zennEntries = zennArticles.map((article: any) => {
+  const zennWorks = zennArticles.map((article: any) => {
     const date = new Date(article.published_at);
     return {
       date: `${date.getFullYear()}-${`0${date.getMonth() + 1}`.slice(-2)}-${`0${date.getDate()}`.slice(-2)}`,
@@ -42,7 +60,7 @@ export async function getOtherWorks() {
     };
   });
 
-  return [...DW_ENTRIES, ...zennEntries].sort((a, b) =>
+  return [...OTHER_WORKS, ...zennWorks].sort((a, b) =>
     a.date > b.date ? -1 : 1
   );
 }
